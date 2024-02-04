5b68eb04bfa26928487cf9a8cb20d41ec51bdd32
ryanmouritz <ryanmouritz@outlook.com>
2024-02-04 17:14:19 +1100

    convert js to ts for @/utils/supabase

--- utils/supabase/component.js
+++ utils/supabase/component.js
@@ -2,8 +2,8 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   )
 
   return supabase
